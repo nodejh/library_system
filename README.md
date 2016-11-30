2dc057aae37a6058a4a019a298b70561ff85634a
nodejh <jianghangscu@gmail.com>
2016-11-30 20:48:20 +0800

    初始化client/

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ## 使用 Node.js + Vue.js 搭建一个图书管理系统
 
+主要目的是学习 Vue.js 以及深入学习 Node.js。
+
 + 后端使用 Node.js，不使用第三方库，自己实现路由功能
 + 前端使用 Vue.js
 + 数据库采用 sqlite3
---
+++ client/src/components/Test.vue
@@ -0,0 +1,14 @@
+<template>
+  <el-radio class="radio" v-model="radio" label="1">备选项</el-radio>
+  <el-radio class="radio" v-model="radio" label="2">备选项</el-radio>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        radio: '1'
+      };
+    }
+  }
+</script>
